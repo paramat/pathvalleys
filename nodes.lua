30b69f5d467bd58f17bb6b3eb211e6d1555ccad8
paramat <mat.gregory@virginmedia.com>
2017-04-17 23:16:31 +0100

    Initial commit

---
+++ nodes.lua
@@ -0,0 +1,151 @@
+minetest.register_node("pathvalleys:junglewood", {
+	description = "Mod jungle wood",
+	tiles = {"default_junglewood.png"},
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:bridgewood", {
+	description = "Bridge wood",
+	tiles = {"pathv7_bridgewood.png"},
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairn", { -- stair rising to the north
+	description = "Jungle wood stair N",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{-0.5, 0, 0, 0.5, 0.5, 0.5},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairs", {
+	description = "Jungle wood stair S",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{-0.5, 0, -0.5, 0.5, 0.5, 0},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:staire", {
+	description = "Jungle wood stair E",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{0, 0, -0.5, 0.5, 0.5, 0.5},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairw", {
+	description = "Jungle wood stair W",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{-0.5, 0, -0.5, 0, 0.5, 0.5},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairne", {
+	description = "Jungle wood stair NE",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{0, 0, 0, 0.5, 0.5, 0.5},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairnw", {
+	description = "Jungle wood stair NW",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{-0.5, 0, 0, 0, 0.5, 0.5},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairse", {
+	description = "Jungle wood stair SE",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{0, 0, -0.5, 0.5, 0.5, 0},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("pathvalleys:stairsw", {
+	description = "Jungle wood stair SW",
+	tiles = {"default_junglewood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			{-0.5, 0, -0.5, 0, 0.5, 0},
+		},
+	},
+	sounds = default.node_sound_wood_defaults(),
+})
